7fadc26d50cac86c4f473166e18acff97788e699
AlvaradoAdam15 <adamalvarado@iesebre.com>
2015-12-16 20:28:25 +0100

    First intent load image

--- app/src/main/java/com/iesebre/dam2/adam/loadimage/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/adam/loadimage/MainActivity.java
@@ -1,13 +1,45 @@
 package com.iesebre.dam2.adam.loadimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.imageMostra);
+
+    }
+
+    private Bitmap loadImageFromNetwork(String url){
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void onClick(View v) {
+        new Thread(new Runnable() {
+            public void run() {
+                Bitmap b = loadImageFromNetwork("http://www.techotopia.com/images/2/21/Android_process_priorities.png");
+                mImageView.setImageBitmap(b);
+            }
+        }).start();
     }
 }
